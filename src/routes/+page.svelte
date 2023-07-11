5b0981824e8f66af514ebe5a307b8078fd90a8ed
damien.fayol <damien.fayol@consonanceweb.com>
2023-07-11 13:38:50 +0200

    Add basic inputs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,44 @@
+<script>
+	import { name, race, kingdom, career, zaifas } from "../store";
+</script>
+
 <div class="absolute w-full top-[-20px]">
     <img src="cs-top.jpg" />
     <img src="cs-bottom.jpg" />
 </div>
+
+<div class="absolute">
+    <input
+        type="text" bind:value={$name}
+        class="top-[32px] left-[80px]"
+    />
+    <input
+        type="text" bind:value={$race}
+        class="top-[32px] left-[240px]"
+    />
+    <input
+        type="text" bind:value={$kingdom}
+        class="top-[50px] left-[80px]"
+    />
+    <input
+        type="text" bind:value={$career}
+        class="top-[50px] left-[240px]"
+    />
+    <input
+        type="number" bind:value={$zaifas}
+        class="top-[936px] left-[295px]"
+    />
+</div>
+
+<style>
+    input {
+        position: absolute;
+        width: 5rem;
+        height: 15px;
+        border: 1px solid #ccc;
+    }
+
+    input[type=number] {
+        width: 3rem;
+    }
+</style>
---
+++ src/store.ts
@@ -0,0 +1,8 @@
+import { writable } from "svelte/store";
+
+export const name = writable('');
+export const race = writable('');
+export const kingdom = writable('');
+export const career = writable('');
+
+export const zaifas = writable(15);
