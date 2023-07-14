ecae6d6ea25526edc52cc40066871f1344ba4843
unknown <dam.fayol@gmail.com>
2023-07-14 16:16:30 +0200

    Code cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,3 @@
 </script>
 
 <App />
-
-<div class="w-full top-[-20px]">
-    <img src="cs-top.jpg" alt="" />
-    <img src="cs-bottom.jpg" alt="" />
-</div>
-
-<!-- <div class="absolute">
-    <input
-        type="number" bind:value={$zaifas}
-        class="top-[936px] left-[295px]"
-    />
-</div> -->
