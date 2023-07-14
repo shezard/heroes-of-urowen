a9306cba45078f0015ec86cf68531fbec5fdbfb1
unknown <dam.fayol@gmail.com>
2023-07-14 13:16:57 +0200

    Move more stats to css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script>
 	import App from "../components/App.svelte";
-	import { name, race, kingdom, career, zaifas } from "../store";
 </script>
 
 <App />
@@ -12,36 +11,7 @@
 
 <!-- <div class="absolute">
     <input
-        type="text" bind:value={$name}
-        class="top-[32px] left-[80px]"
-    />
-    <input
-        type="text" bind:value={$race}
-        class="top-[32px] left-[240px]"
-    />
-    <input
-        type="text" bind:value={$kingdom}
-        class="top-[50px] left-[80px]"
-    />
-    <input
-        type="text" bind:value={$career}
-        class="top-[50px] left-[240px]"
-    />
-    <input
         type="number" bind:value={$zaifas}
         class="top-[936px] left-[295px]"
     />
 </div> -->
-
-<style>
-    input {
-        position: absolute;
-        width: 5rem;
-        height: 15px;
-        border: 1px solid #ccc;
-    }
-
-    input[type=number] {
-        width: 3rem;
-    }
-</style>
