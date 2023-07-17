260f411fdc28f494f5af114d6f2dc7deb4c8b9b7
unknown <dam.fayol@gmail.com>
2023-07-17 08:33:30 +0200

    Add button to remove xp, increase xp button gutter

--- src/components/XP.svelte
+++ src/components/XP.svelte
@@ -8,11 +8,21 @@
             return store;
         });
     };
+
+    const removeXP = function () {
+        store.update((store) => {
+            store.XP -= 1;
+            store.totalXP -= 1;
+            return store;
+        });
+    };
 </script>
 
 <span class="flex">
     <span class="px-1">
         XP : {$store.XP}
-        <span on:click={addXP} on:keypress={addXP} role="button" tabindex="0"> [+] </span>
+        <span on:click={addXP} on:keypress={addXP} role="button" tabindex="0"> [ + ] </span>
+        <span class="px-1" />
+        <span on:click={removeXP} on:keypress={removeXP} role="button" tabindex="0"> [ - ] </span>
     </span>
 </span>
